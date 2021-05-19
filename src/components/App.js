9d1790edf14f0a0b7579900ed4d9ebb7de61cf2a
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-05-19 15:17:08 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,24 @@ import "../styles/App.css";
 
 const App = () => {
   let [count, setCount] = useState(0);
-  let dClick = useEffect(() => {
-    alert("cant edit it");
-  });
+
   let sCount = () => {
     setCount(count + 1);
   };
+
+  let dClick = () => {
+    alert("cant edit it");
+  };
+
   // let dClick = () => {
   //   alert("cant edit it");
   // };
   return (
     <div className="ball">
-      <h1 className="count">{count}</h1>
-      <button
-        className="increment-button"
-        onClick={sCount}
-        onDoubleClick={dClick}
-      >
+      <h1 className="count" onDoubleClick={dClick}>
+        {count}
+      </h1>
+      <button className="increment-button" onClick={sCount}>
         increment
       </button>
     </div>
